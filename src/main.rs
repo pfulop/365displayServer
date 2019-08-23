4e45cbed1581617978419e6385ea9ed766855c9a
Pavol Fulop <pavolfulop@gmail.com>
2019-08-23 08:43:40 +0200

    sentry

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use lambda_runtime::{error::HandlerError, lambda, Context};
+use log::info;
+use serde::Serialize;
 use serde_json::Value;
 
-fn main()  {
-    lambda!(handler)
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CustomOutput {
+    status_code: i16,
 }
 
-fn handler(event: Value, _: Context) -> Result<Value, HandlerError> {
-    Ok(event)
+fn main() {
+    let _guard = sentry::init("https://fbd344796c774cc9a67a908d0781a9d4@sentry.io/1539573");
+    info!("main");
+    return lambda!(handler);
+}
+
+fn handler(_event: Value, _: Context) -> Result<CustomOutput, HandlerError> {
+    info!("handler");
+    return Ok(CustomOutput { status_code: 200 });
 }
